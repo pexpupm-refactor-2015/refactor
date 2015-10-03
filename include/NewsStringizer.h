7451a003b8d4d0aaf34b9922cb17190d35a25000
sarroutbi <sarroutbi@yahoo.es>
2015-10-03 12:47:53 +0200

    Adding method to ensure that Analyzer is parsing Tuits correctly (body included)

---
+++ include/NewsStringizer.h
@@ -0,0 +1,21 @@
+/*
+ * Analyzer.h
+ */
+#ifndef _NEWS_STRINGIZER_H_
+#define _NEWS_STRINGIZER_H_
+
+#include <string>
+#include <list>
+#include "New.h"
+
+class NewsStringizer
+{
+public:
+  // Dump news titles to string
+  static std::string titlesToString(const std::list<New>& news_list);
+
+  // Dump news titles and bodies to string
+  static std::string titlesAndBodiesToString(const std::list<New>& news_list);
+};
+
+#endif /* _NEWS_STRINGIZER_H_ */
